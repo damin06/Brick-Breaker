0815465da519a073910e75d5e25b0b692860f600
KoDaMin <ko060614@gmail.com>
2023-02-25 23:20:12 +0900

    remake

---
+++ Brick_Breaker_Remake/Assets/02_Scripts/BlockSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField] private Vector3[] blockPos;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
